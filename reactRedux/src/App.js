d55bf2eb566caa4db2d05e7a3f9a4ef7a0232119
Lucas <l.duarte.mk@gmail.com>
2019-07-14 12:42:00 -0300

    atualização App.Js Initial

--- reactRedux/src/App.js
+++ reactRedux/src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
-import React, { PureComponent }from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux'
 
 import 'milligram'
 
 class App extends PureComponent {
-  render () {
-   return(
-    <h1>Workspace</h1>
+  render() {
+    return (
+      <div>
+        <table>Table 1</table>
+        <lu>
+          <li>item 1 Tabela 1</li>
+          <li>item 2 Tabela 1</li>
+          <li>item 3 Tabela 1</li>
+          <button>Tabela 1</button>
+        </lu>
+        <table>Table 2</table>
+        <lu>
+          <li>item 1 Tabela 2</li>
+          <li>item 2 Tabela 2</li>
+          <li>item 3 Tabela 2</li>
+          <button>Tabela 2</button>
+        </lu>
+        <table>Table 3</table>
+        <lu>
+          <li>item 1 Tabela 3</li>
+          <li>item 2 Tabela 3</li>
+          <li>item 3 Tabela 3</li>
+          <button>Tabela 3</button>
+        </lu>
+      </div>
     )
-   }
+  }
 }
 
 //
