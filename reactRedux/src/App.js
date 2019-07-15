2d9f41d0f775d137bdc7041140608216eaf47bc9
Lucas <l.duarte.mk@gmail.com>
2019-07-15 13:59:39 -0300

    create Form

--- reactRedux/src/App.js
+++ reactRedux/src/App.js
@@ -1,40 +1,57 @@
 import './App.css';
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux'
+import styled from 'styled-components'
 
 import 'milligram'
 
 class App extends PureComponent {
   render() {
     return (
-      <div>
-        <table>Table 1</table>
-        <lu>
-          <li>item 1 Tabela 1</li>
-          <li>item 2 Tabela 1</li>
-          <li>item 3 Tabela 1</li>
-          <button>Tabela 1</button>
-        </lu>
-        <table>Table 2</table>
-        <lu>
-          <li>item 1 Tabela 2</li>
-          <li>item 2 Tabela 2</li>
-          <li>item 3 Tabela 2</li>
-          <button>Tabela 2</button>
-        </lu>
-        <table>Table 3</table>
-        <lu>
-          <li>item 1 Tabela 3</li>
-          <li>item 2 Tabela 3</li>
-          <li>item 3 Tabela 3</li>
-          <button>Tabela 3</button>
-        </lu>
-      </div>
+      <Container>
+        <Div>
+          <ul>
+            <span>Table 1</span>
+            <li>item 1 Tabela 1</li>
+            <li>item 2 Tabela 1</li>
+            <li>item 3 Tabela 1</li>
+            <button>Tabela 1</button>
+          </ul>
+        </Div>
+        <Form>
+          <span htmlFor="id" name="id" id="id">Id</span>
+          <input type="text" id="id" name="id" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Container>
     )
   }
 }
 
-//
+const Div = styled.div`
+border: 1px solid;
+width: 500px;
+height 300px;
+padding: 10px;
+`
+
+const Container = styled.div`
+display: flex;
+justify-content: space-between;
+padding 10px;
+flex-wrap: wrap;
+flex-direction: row;
+`
+
+const Form = styled.form`
+border: 1px solid;
+justify-content: space-between;
+width: 500px;
+height 300px;
+display: flex;
+
+`
+
 
 const mapStateToProps = (state) => ({
 })
